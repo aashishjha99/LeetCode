bccf13ffa55ed76db1b2f222ec744f028b5fed4b
Aashish <60545932+aashishjha99@users.noreply.github.com>
2022-02-07 19:21:50 +0530

    Time: 12 ms (28.23%), Space: 13 MB (46.34%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -5,8 +5,8 @@ public:
        int sum = 0;
         
         for(int i = 1; i < prices.size(); i++)
-            if(prices[i] > prices[i - 1])
-                sum += prices[i] - prices[i - 1];
+            if(prices[i] > prices[i - 1])     // conditon for checking whether the element                                               //   selling price is greater than purchased
+                sum += max(prices[i] - prices[i - 1],0);
         
         return sum;
 
